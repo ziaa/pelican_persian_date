70f1a7638e7947f04b20050970690214777811db
Ziaa <s.zia.azimi@gmail.com>
2015-07-28 12:03:06 +0430

    Add license

--- persian_date.py
+++ persian_date.py
@@ -1,4 +1,28 @@
 # -*- coding:utf-8 -*-
+"""
+Pelican Persian date
+====================
+A Pelican plugin to convert 'article.locale_date' attribute
+from Gregorian calendar into Solar Hijri calendar (AKA Jalali calendar,
+Persian calendar, iranian calendar) which is the official calendar of
+Iran and Afghanistan.
+"""
+# Pelican Persian date
+# Copyright (C) 2015  Seyed Zia Eddin Azimi
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 from .jdatetime import datetime
 from pelican import signals
 
