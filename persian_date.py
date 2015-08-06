9cf119cce8e9e88e764ae4721709500d8734ef04
Ziaa <s.zia.azimi@gmail.com>
2015-08-06 17:14:52 +0430

    Check the file with flake8

--- persian_date.py
+++ persian_date.py
@@ -26,9 +26,13 @@ Iran and Afghanistan.
 from .jdatetime import datetime
 from pelican import signals
 
+
 def article_persian_date(article_generator):
-	for article in article_generator.dates:
-		article.locale_date = datetime.fromgregorian(day=article.date.day,month=article.date.month,year=article.date.year).strftime(article.date_format)
+    for article in article_generator.dates:
+        article.locale_date = datetime.fromgregorian(
+                        day=article.date.day, month=article.date.month,
+                        year=article.date.year).strftime(article.date_format)
+
 
 def register():
-	signals.article_generator_finalized.connect(article_persian_date)
+    signals.article_generator_finalized.connect(article_persian_date)
